7d6ab7c9286fe99d17fd97063493eccf8b1271dd
shivangi-tech-31 <trivedishivangi286@gmail.com>
2025-08-07 11:58:12 +0000

    Ctrl+Enter to commit on main

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId=1334
 let accountEmail="hitesh"
 var accountPassword="234"
 accountCity="jaipur"
+let accountState;
 
 //accountId=2
 
@@ -15,5 +16,13 @@ console.log(accountPassword);
 console.log(accountCity);
 
 //to show whole values in table.
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/ 
+
+/*only use const or let. you should not declare a variable without let or var */
+
+//if u only declare a variable then by default it is initialized undefined 
