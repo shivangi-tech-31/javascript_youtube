c3f3e09b93ee8088228bee3caa6fde5af6dd950d
shivangi-tech-31 <trivedishivangi286@gmail.com>
2025-08-07 12:15:30 +0000

    ctrl+enter to commit in main

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+/*
+alert("hello")
+alert(3+3)
+we r using node js and not browser thats whyalert function will display error in the terminal
+*/
+
+//code should be readable 
