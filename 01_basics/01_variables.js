5ec16cfcdd2c0af7c815133108aca89785274b68
shivangi-tech-31 <trivedishivangi286@gmail.com>
2025-08-07 11:40:16 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=1334
+let accountEmail="hitesh"
+var accountPassword="234"
+accountCity="jaipur"
+
+//accountId=2
+
+accountEmail="hiufid@gmail.com"
+accountPassword="543"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+//to show whole values in table.
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
