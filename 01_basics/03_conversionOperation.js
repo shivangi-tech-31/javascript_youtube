623db8eaa7bea0778c70113f6779fdd5145f666e
shivangi-tech-31 <trivedishivangi286@gmail.com>
2025-08-22 01:32:07 +0000

    Ctrl+Enter to commit on main

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,4 +12,46 @@ let valueInNumber=  Number(score)
 let isLoggedIn=1
 
 let booleanisloggedin= Boolean(isLoggedIn)
-console.log(booleanisloggedin);
+//console.log(booleanisloggedin);
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//**************operations ***************** 
+
+ let value=2
+ let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //power
+// console.log(2/2); //quotient
+// console.log(2%2); //remainder
+
+let str1="hello"
+let str2="hitesh"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);//12
+// console.log("1"+"2");//12
+// console.log(1+"2");//12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2");//32
+
+// console.log(+true); //1
+// console.log(true+);//error
+// console.log(+"");//0
+
+let gc=3
+// console.log(gc++);
+// console.log(gc);
+
+
